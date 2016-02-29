7b3b184caecf25c9fd172d6049695728e8e14bd3
ThiagoLuizNunes <thiago.luiz@lavid.ufpb.br>
2016-02-29 18:21:54 -0300

    Google Question so close to finish

--- GoogleQuestion.c
+++ GoogleQuestion.c
@@ -2,22 +2,23 @@
 #include <stdlib.h>
 
 typedef struct {
-	int arrayPOS[10];
-	int arrayNEG[10];
+	int *arrayPOS;
+	int *arrayNEG;
 	int numbPOS;
 	int numbNEG;
 }tLista;
 
-void ordenaLista(tLista *list){
+
+void ordenaLista(tLista list){
 	int i, x;
 	
 	printf("\n");
 	
-	for(i=0; i<list->numbNEG; i++){
-		printf("%d ", list->arrayNEG[i]);
+	for(i=0; i<list.numbNEG; i++){
+		printf("%d ", list.arrayNEG[i]);
 	}
-	for(x=0; x<list->numbPOS; x++){
-		printf("%d ", list->arrayPOS[x]);
+	for(x=0; x<list.numbPOS; x++){
+		printf("%d ", list.arrayPOS[x]);
 	}
 	
 	printf("\n");
@@ -25,27 +26,31 @@ void ordenaLista(tLista *list){
 
 int main(void){
 	
-	printf("Hello word\n");
+	int array[] = {1,5,0,-1,-2,-3,0,-1,7,-2};
+	int i, z=0, w=0;
 	
-	int array[] = {0,1,2,-2,6,2,-2,3,0,-1};
-	int i, z = 0, w = 0;
+	tLista lista;
+	lista.arrayNEG = malloc(10 * sizeof *lista.arrayNEG);
+	lista.arrayPOS = malloc(10 * sizeof *lista.arrayPOS);
 	
-	tLista *lista = malloc(sizeof(tLista));
-	
-	for(i=0; i<10; i++){
+	for(i=0; i<sizeof(array)/4; i++){
 		if(array[i] < 0){
-			lista->arrayNEG[z] = array[i];
+			lista.arrayNEG[z] = array[i];
 			z++;
-			lista->numbNEG++;
+			lista.numbNEG = z;
 		}
+		
 		else{
-			lista->arrayPOS[w] = array[i];
+			lista.arrayPOS[w] = array[i];
 			w++;
-			lista->numbPOS++;
+			lista.numbPOS = w;
 		}
 	}
 	
 	ordenaLista(lista);
+	
+	free(lista.arrayNEG);
+	free(lista.arrayPOS);
 	
 	return 0;
 }
