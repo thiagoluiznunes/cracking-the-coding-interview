516bc8941d1fd2a393a28ab4fa90bb531459c592
ThiagoLuizNunes <thiago.luiz@lavid.ufpb.br>
2016-03-17 17:48:42 -0300

    update question

---
+++ AmazonQuestionList.c
@@ -0,0 +1,38 @@
+/*
+Ex2 [Amazon]: Given a singly link list and a number K; swap the Kth node from the start with the Kth node from the last. Check all the edge cases.
+Sample Input: 1->;2->;3->;4->;5->;6->;7->;8 and K = 3
+Sample Output : 1->;2->;6->;4->;5->;3->;7->;8
+Sample Input: 1->;2->;3->;4->;5->;6->;7->;8 and K = 10
+Sample Output: print error "LIST IS OF LESSER SIZE";
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "Lista.h"
+
+
+
+int main(void){
+	
+	tLista lista;
+
+	createList(&lista);
+
+	int array[] = {1,2,3,4,5,6,7,8,9};
+	int i, x = 0;
+	for(i=0; i<sizeof(array)/4; i++){
+		x = insertElement(&lista, i+1, array[i]);
+	}
+
+	printList(&lista);
+	changePosition(&lista, 2);
+	printList(&lista);
+	changePosition(&lista, 2);
+	printList(&lista);
+	changePosition(&lista, 5);
+	printList(&lista);
+	changePosition(&lista, 5);
+	printList(&lista);
+	return 0;
+}
--- GoogleQuestionList.c
+++ GoogleQuestionList.c
@@ -1,3 +1,14 @@
+/*
+[Google] Give you an array which has n integers,
+it has both positive and negative integers. 
+Now you need sort this array in a special way.
+After that,the negative integers should in the front,
+and the positive integers should in the back.
+Also the relative position should not be changed.
+eg. -1 1 3 -2 2 ans: -1 -2 1 3 2.
+O(n)time complexity and O(1) space complexity is perfect.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
