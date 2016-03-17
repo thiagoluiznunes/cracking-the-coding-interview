443d6b327633d6d6de50918db159c9077c095a38
ThiagoLuizNunes <thiago.luiz@lavid.ufpb.br>
2016-03-17 17:23:00 -0300

    changes some question name and add new question Amazon using Stack and recursion

---
+++ GoogleQuestionStack.c
@@ -0,0 +1,17 @@
+/*
+[Google] 
+Write a program that implements a 
+Doubly Linked List using Stacks with 
+min complexity.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+	
+	
+	
+	system("pause");
+	return 0;
+}
